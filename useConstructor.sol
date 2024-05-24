db62682ac514ec20e23a68abb56a7b97159e4e3c
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2024-05-24 10:45:56 +0530

    constructor.sol

---
+++ useConstructor.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.24 and less than 0.9.0
+pragma solidity ^0.8.24;
+
+contract HelloWorld {
+    string public greet ;
+    uint256 public no;
+
+    constructor(string memory _hey,uint256 _no){
+        greet = _hey;
+        no = _no;
+    }
+}
