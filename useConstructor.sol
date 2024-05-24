135ca2dbeff7abeae1721eb5fbba5ea920258e95
Tushar-Ranjan-Sahoo <94734267+Tushar-Ranjan-Sahoo@users.noreply.github.com>
2024-05-24 10:52:34 +0530

    function inputs

--- useConstructor.sol
+++ useConstructor.sol
@@ -6,8 +6,12 @@ contract HelloWorld {
     string public greet ;
     uint256 public no;
 
-    constructor(string memory _hey,uint256 _no){
-        greet = _hey;
-        no = _no;
+  // constructor(string memory _hey,uint256 _no){
+    //     greet = _hey;
+    //     no = _no;
+    // }
+    function addInfo(string memory _hey,uint256 _no) public {
+        greet =_hey;
+        no= _no;
     }
 }
